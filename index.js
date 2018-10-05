02c93302fdbe7e64c747f4398cc260479a51a42e
Joshua McKearin <josh@mckearin.codes>
2018-10-04 21:08:23 -0500

    refactor index.js

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const cTable = require('console.table');
 
 const scoresPage = {
   uri: 'https://leagues.bluesombrero.com/Default.aspx?tabid=689478&ProgramId=80037731&DivisionId=80255584',
-  // uri: `https://leagues.bluesombrero.com/Default.aspx`,
-  // tabid: 689478,
-  // ProgramId: 80037731,
-  // DivisionId: 80255584,
   callback: parseScoresTable
 }
 
@@ -37,36 +33,10 @@ function parseScoresTable(err, res, done) {
   if (err) {
     console.log(err)
   } else {
-    const $ = res.$;
-    $('.rgMasterTable tbody tr').each((i, row) => {
-      const children = $(row).children('td');
-      const date = $(children[1]).text();
-      const [startTime, endTime] = $(children[2]).text().split(' - ');
-      const {awayScore, homeScore} = parseScore($(children[6]).text());
-      const game = {
-        startDate: moment(`${date} ${startTime}`, 'MM/DD/YYYY HH:mmA').utcOffset('-06:00'),
-        endDate:  moment(`${date} ${endTime}`, 'MM/DD/YYYY HH:mmA').utcOffset('-06:00'),
-        location: $(children[3]).text(),
-        awayTeam: $(children[4]).text(),
-        homeTeam: $(children[5]).text(),
-        awayScore: Math.trunc(awayScore),
-        homeScore: Math.trunc(homeScore)
-      };
-      games.insert(game);
-      db.saveDatabase();
-    });
+    getAllGames(res);
     getAllTeams(games.data);
+
     let results = [];
-    // let team = teams.findOne({name: 'Bordeaux PW - Green'});
-    // const teamGames = getAllGamesForTeam(team);
-    //   const {totalWins, totalLosses} = getTeamWinLosses(teamGames, team);
-    //   results.push({
-    //     name: team.name,
-    //     totalWins,
-    //     totalLosses,
-    //     winPercentage: (totalWins / (totalWins + totalLosses)).toFixed(3),
-    //     rpi: calculateRPI(teamGames, team),       
-    //   })
     teams.data.forEach(team => {
       const teamGames = getAllGamesForTeam(team);
       const {totalWins, totalLosses} = getTeamWinLosses(teamGames, team);
@@ -79,9 +49,36 @@ function parseScoresTable(err, res, done) {
       })
     });
     results.sort((a,b) => (a.rpi < b.rpi) ? 1 : ((b.rpi < a.rpi) ? -1 : 0));
+
     console.table(results);
   }
   done();
+}
+
+function parseScore(score) {
+  const [awayScore, homeScore] = score.split(' - ');
+  return {awayScore, homeScore};
+}
+
+function getAllGames(res) {
+  const $ = res.$;
+  $('.rgMasterTable tbody tr').each((i, row) => {
+    const children = $(row).children('td');
+    const date = $(children[1]).text();
+    const [startTime, endTime] = $(children[2]).text().split(' - ');
+    const { awayScore, homeScore } = parseScore($(children[6]).text());
+    const game = {
+      startDate: moment(`${date} ${startTime}`, 'MM/DD/YYYY HH:mmA').utcOffset('-06:00'),
+      endDate: moment(`${date} ${endTime}`, 'MM/DD/YYYY HH:mmA').utcOffset('-06:00'),
+      location: $(children[3]).text(),
+      awayTeam: $(children[4]).text(),
+      homeTeam: $(children[5]).text(),
+      awayScore: Math.trunc(awayScore),
+      homeScore: Math.trunc(homeScore)
+    };
+    games.insert(game);
+    db.saveDatabase();
+  });
 }
 
 function getAllTeams(data) {
@@ -94,15 +91,10 @@ function getAllTeams(data) {
   db.saveDatabase();
 }
 
-function parseScore(score) {
-  const [awayScore, homeScore] = score.split(' - ');
-  return {awayScore, homeScore};
-}
-
 function calculateRPI(teamGames, team) {
   const teamWP = getTeamWP(teamGames, team);
-  const {owp: teamOWP, oowp: teamOOWP} = getTeamOWP(teamGames, team);
-  // const teamOOWP = getTeamOOWP(teamGames, team);
+  const teamOWP = getTeamOWP(teamGames, team);
+  const teamOOWP = getTeamOOWP(teamGames, team);
   const teamRPI = (0.4 * teamWP + 0.4 * teamOWP + 0.2 * teamOOWP).toFixed(4);
   return teamRPI;
 }
@@ -139,13 +131,24 @@ function getTeamWP(teamGames, team, teamToIgnore) {
 }
 
 function getTeamOWP(teamGames, team) {
-  let owps = [], oowps = [];
+  let owps = [];
   teamGames.forEach(game => {
     const opponentTeamName = getOpponentTeamName(game, team);
     const opponentTeamGames = getAllGamesForTeam({name: opponentTeamName});
+    // this would calculate the WP without using games against the team we are considering
     // const owp = getTeamWP(opponentTeamGames, {name: opponentTeamName}, team);
     const owp = getTeamWP(opponentTeamGames, {name: opponentTeamName});
     owps.push(+owp);
+  });
+
+  return +(owps.reduce((total, owp) => total + owp) / owps.length).toFixed(3);
+}
+
+function getTeamOOWP(teamGames, team) {
+  let oowps = [];
+  teamGames.forEach(teamGame => {
+    const opponentTeamName = getOpponentTeamName(teamGame, team);
+    const opponentTeamGames = getAllGamesForTeam({name: opponentTeamName});
     opponentTeamGames.forEach(opponentTeamGame => {
       const ooTeamName = getOpponentTeamName(opponentTeamGame, opponentTeamName);
       const ooTeamGames = getAllGamesForTeam({name: ooTeamName});
@@ -154,22 +157,8 @@ function getTeamOWP(teamGames, team) {
     });
   });
 
-  return {
-    owp: +(owps.reduce((total, owp) => total + owp) / owps.length).toFixed(3),
-    oowp: +(oowps.reduce((total, oowp) => total + oowp) / oowps.length).toFixed(3)
-  };
+  return +(oowps.reduce((total, oowp) => total + oowp) / oowps.length).toFixed(3);
 }
-
-// function getTeamOOWP(teamGames, team) {
-//   let;
-//   teamGames.forEach(teamGame => {
-//     const opponentTeamName = getOpponentTeamName(teamGame, team);
-//     const opponentTeamGames = getAllGamesForTeam({name: opponentTeamName});
-    
-//   });
-
-//   return +(oowps.reduce((total, oowp) => total + oowp) / oowps.length).toFixed(3);
-// }
 
 function getAllGamesForTeam({name}) {
   return games.where(game => {
